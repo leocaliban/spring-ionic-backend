79b9d8ea75854f13f8ca6a6b2065840db724820a
Leocaliban <leocaliban@gmail.com>
2018-03-10 21:40:46 -0300

    Cria o profile de Desenvolvimento

--- src/main/java/com/leocaliban/loja/config/DevConfig.java
+++ src/main/java/com/leocaliban/loja/config/DevConfig.java
@@ -3,6 +3,7 @@ package com.leocaliban.loja.config;
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -10,17 +11,21 @@ import org.springframework.context.annotation.Profile;
 import com.leocaliban.loja.services.DBService;
 
 /**
- * Classe {@link TestConfig} responsável pelas configurações específicas do profile de testes
+ * Classe {@link DevConfig} responsável pelas configurações específicas do profile de desenvolvimento
  * @author Leocaliban
  *
  * 10 de mar de 2018
  */
 @Configuration
-@Profile("test")
-public class TestConfig {
+@Profile("dev")
+public class DevConfig {
 	
 	@Autowired
 	private DBService dbService;
+	
+	//recupera o valor de application-dev.properties
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
 	
 	/**
 	 * Instancia o banco de dados para meios de testes
@@ -29,6 +34,9 @@ public class TestConfig {
 	 */
 	@Bean
 	public boolean instantiateDatabase() throws ParseException {
+		if(!"create".equals(strategy)) {
+			return false;
+		}
 		dbService.instantiateTestDatabase();	
 		return true;
 	}
